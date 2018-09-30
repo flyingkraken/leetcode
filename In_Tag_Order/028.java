beeb2259363c43c26e06c8792eeffb30c55fddda
MacBook <Macbook@macdeMacBook-Pro.local>
2018-09-30 12:04:52 -0700

    using Java's API

---
+++ In_Tag_Order/028.java
@@ -0,0 +1,20 @@
+//Using Java's API
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0) return 0;
+        
+        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+            if(haystack.substring(i,i+needle.length()).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+//one line version....(also using Java's API)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
